127c3cbc592912b92c22fc3e95a643be0ae90f0f
Alan <alan88w@yahoo.com>
2020-08-29 22:26:56 +0800

    Initial Commit

---
+++ LearningCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearningCPP
+//
+//  Created by Alan Wan on 29/8/20.
+//  Copyright © 2020 Alan Wan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
